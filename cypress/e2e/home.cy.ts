f220b343fa15c8215ba0669db3d2ac01fde53bda
devofthings <chris-winter@live.de>
2022-08-02 14:27:22 +0200

    ✨ added startdates to the races

--- cypress/e2e/home.cy.ts
+++ cypress/e2e/home.cy.ts
@@ -27,6 +27,7 @@ describe('Ponyracer', () => {
   it('should display a race list', () => {
     cy.visit('/');
     cy.get('h2').should('have.length', 2);
+    cy.get('p').should('have.length', 2);
   });
 
   it('should display ponies', () => {
--- src/app/race/race.component.spec.ts
+++ src/app/race/race.component.spec.ts
@@ -26,7 +26,7 @@ describe('RaceComponent', () => {
         { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
         { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
       ],
-      startInstant: '2020-02-18T08:02:00Z'
+      startInstant: '2022-02-18T08:02:00'
     };
 
     // when triggering the change detection
@@ -37,6 +37,9 @@ describe('RaceComponent', () => {
     const raceName = element.querySelector('h2');
     expect(raceName).withContext('You need an h2 element for the race name').not.toBeNull();
     expect(raceName.textContent).withContext('The h2 element should contain the race name').toContain('Paris');
+    const startInstant = element.querySelector('p');
+    expect(startInstant).withContext('You need an p element for the race start instant').not.toBeNull();
+    expect(startInstant.textContent).withContext('The p element should contain the race start instant').toContain('2/18/22, 8:02 AM');
     const ponies = fixture.debugElement.queryAll(By.directive(PonyComponent));
     expect(ponies).withContext('You should use the PonyComponent in your template to display the ponies').not.toBeNull();
     expect(ponies.length).withContext('You should have five pony components in your template').toBe(5);
